740d4419dbb1b593c18f64b4836781a0699f08ee
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-05-31 11:01:00 +0530

    COMMIT_EDITMSG

--- js/higherorder.js
+++ js/higherorder.js
@@ -2,5 +2,7 @@ const result=[1,2,3,4,5,6,7,8,9];
 const sqresult=result.map((num)=> num*num);
 console.log(sqresult);
 
-const result1=["orange","apple","banana","kiwi"];
-
+const result1=["orange","apple","banana","kiwi",];
+const searchTerm = "ap";
+filterresult=result1.filter((fruit)=> fruit.includes(searchTerm));
+console.log(filterresult);
